821308e5421e23b42aba15906b747bc87533864a
VineshDeloIn <42538826+VineshDeloIn@users.noreply.github.com>
2018-08-22 11:15:39 +0530

    Modified gruntFile updated package.json --vinesh

--- gruntFile.js
+++ gruntFile.js
@@ -1,19 +1,45 @@
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
-     
         pkg: grunt.file.readJSON('package.json'),
-
-        uglify : {
-            uglifyJS : {
-                files : [{
-                    src: '**/*.js',
-                    dest :'dest/all.min.js'  
-                }]
+        concat: {
+            css: {
+                src: ['./Assets/css/**/*.css'],
+                dest: 'dist/css/style.css'
+            },
+            js: {
+                src: ['./Assets/css/**/*.js'],
+                dest: 'dist/js/script.js'
             }
-        }  
-    
-    })
+        },
+        cssmin: {
+            css: {
+                src: './dist/css/style.css',
+                dest: './dist/css/min/style.min.css'
+            }
+        },
+        uglify: {
+            js: {
+                src: './dist/js/script.js',
+                dest: './dist/js/min/script.min.js'
+            }
+        },
+        watch: {
+            css: {
+                files: ['./Assets/css/**/*.css'],
+                tasks: ['concat:css', 'cssmin:css']
+            },
+            js: {
+                files: ['./Assets/js/**/*.js'],
+                tasks: ['concat:js', 'uglify:js']
+            }
+        }
+    });
+
+
   };
