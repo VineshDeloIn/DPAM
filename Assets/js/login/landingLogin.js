b1595c0b6bb2d22e59dd1b11a4a35ce95ff46591
VineshDeloIn <42538826+VineshDeloIn@users.noreply.github.com>
2018-08-23 12:07:54 +0530

    Modified LandingLogin.js --vinesh

--- Assets/js/login/landingLogin.js
+++ Assets/js/login/landingLogin.js
@@ -1,11 +1,29 @@
 $( document ).ready(function() {
     
-var username = $("mail").val();
-var password = $("password").val();
+var username = $("#mail").val();
+var password = $("#password").val();
 
 
 
-    $("#loginForm").click(function () {
-     alert(username);
+    $("#loginBtn").click(function () {
+        var username = $("#mail").val();
+        var password = $("#password").val();
+
+        var isUserNameValid;
+        var isPasswordValid;
+        
+        if ($('#remeberMeCbx').is(':checked') && isUserNameValid && isPasswordValid) {
+            // save username and password
+            localStorage.userName =$("#mail").val().val();
+            localStorage.password = $("#password").val();
+            localStorage.checkBoxValidation = $('#remeberMeCbx').val();
+        } else {
+            localStorage.userName = '';
+            localStorage.password = '';
+            localStorage.checkBoxValidation = '';
+        }
     });
+
+    
+
 });
