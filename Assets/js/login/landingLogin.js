e47954236fb9a4990ad52b3fe8b4347df8e13b2e
VineshDeloIn <42538826+VineshDeloIn@users.noreply.github.com>
2018-08-23 13:22:59 +0530

    Updated RememberMe functionality and validations for Login --Vinesh

--- Assets/js/login/landingLogin.js
+++ Assets/js/login/landingLogin.js
@@ -1,20 +1,44 @@
 $( document ).ready(function() {
     
-var username = $("#mail").val();
-var password = $("#password").val();
-
-
+    $("input").focus(function() {
+        $(".mailErr").html("");
+        $(".pswdErr").html("");
+    });
 
     $("#loginBtn").click(function () {
-        var username = $("#mail").val();
+        //getting values from the form
+        var emailMob = $("#mail").val();
         var password = $("#password").val();
 
-        var isUserNameValid;
+        var isEmailMobValid;  
         var isPasswordValid;
+
+        //Checking if email/mob is not empty
+        if(emailMob == "") {
+            $(".mailErr").html("Email/MobileNumber Cannot Be Empty!");
+            isEmailMobValid = false;
+        } else  {
+            isEmailMobValid = true;
+        }
+
+        //Checking if password is not empty
+        if(password == "") {
+            $(".pswdErr").html("Password Cannot Be Empty!");
+            isPasswordValid = false;
+        } else {
+            isPasswordValid = true;
+        }
+
+        if(isEmailMobValid && isPasswordValid) {
+            alert(emailMob + " "+password);
+        }
         
-        if ($('#remeberMeCbx').is(':checked') && isUserNameValid && isPasswordValid) {
+        
+        //added RememberMe functionality of rememberme is checked, Email and password cannot be empty
+        if ($('#remeberMeCbx').is(':checked') && isEmailMobValid && isPasswordValid) {
             // save username and password
-            localStorage.userName =$("#mail").val().val();
+            console.log('Entering!');
+            localStorage.userName =$("#mail").val();
             localStorage.password = $("#password").val();
             localStorage.checkBoxValidation = $('#remeberMeCbx').val();
         } else {
@@ -22,8 +46,11 @@ var password = $("#password").val();
             localStorage.password = '';
             localStorage.checkBoxValidation = '';
         }
-    });
 
+        $('#mail').val('');
+        $('#password').val('');
+        $('#remeberMeCbx').prop('checked', false);
+    });
     
 
 });
