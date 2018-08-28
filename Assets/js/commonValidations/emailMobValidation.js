000b2577fcb13f99e7cc5e65dd22a0702b6fd50f
Surendra <sanushka@deloitte.com>
2018-08-28 12:41:41 +0530

    updated login --anushka

--- Assets/js/commonValidations/emailMobValidation.js
+++ Assets/js/commonValidations/emailMobValidation.js
@@ -1,5 +1,5 @@
 function  validatePhone(txtPhone) {
-    alert("validatePhn");
+  
     var  filter  =  /^(?:\+971|00971|0)?(?:50|51|52|55|56|54|57)\d{7}$/;
     if  (filter.test(txtPhone)) {
         if (txtPhone.length == 10) {
@@ -10,7 +10,7 @@ function  validatePhone(txtPhone) {
 }
 
 function  validateEmail(email) {
-    alert("validateEmail");
+   
     var  emailReg  =  /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     return  emailReg.test( email );
 } 
