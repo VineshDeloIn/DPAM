6c612dfc0a1e6668bb220e5456f458abe638058a
Surendra <sanushka@deloitte.com>
2018-08-29 16:19:23 +0530

    updated sign up --anushka

--- Assets/js/signup.js
+++ Assets/js/signup.js
@@ -22,25 +22,7 @@ if(!validatePhone(email)&&(!validateEmail(email)))
 
 });
 
-//phone validation for Dubai number
 
-function validatePhone(txtPhone) {
-   
-    var filter = /^(?:\+971|00971|0)?(?:50|51|52|55|56|54|57)\d{7}$/;;
-    if (filter.test(txtPhone)) {
-        if(txtPhone.length<=12){
-        return true;
-      }
-      return false;
-    }
-    
-}
-// email validation 
-
-function validateEmail(email) {
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    return emailReg.test( email );
-  }
 
 // function to show password
 
