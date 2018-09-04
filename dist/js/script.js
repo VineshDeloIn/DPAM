5813059ca8a980fbcf379d5e98c04a48544a8917
Surendra <sanushka@deloitte.com>
2018-09-04 10:36:20 +0530

    testing validation --anushka

--- dist/js/script.js
+++ dist/js/script.js
@@ -11940,7 +11940,7 @@ function validatePassword(inputValue){
 // function for password tooltip Color Change
 
 
-function changeTooltipColor(inputValue){
+function changeTooltipColor(inputValue,e){
     
     var lowerCaseLetters = /[a-z]/g;
     var specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
@@ -11948,8 +11948,8 @@ function changeTooltipColor(inputValue){
     var upperCaseLetters = /[A-Z]/g;
 
    var spanTick = $("<span>&#10004;</span>");
-   var spanRemove = $("<span>&#10006;</span>");
-   
+   var spanRemove = $("<span>&#10060;</span>");
+ 
   
  
    // later, when your loading is done:
@@ -11962,44 +11962,62 @@ function changeTooltipColor(inputValue){
       
         
     }
+
     if(lowerCaseLetters.test(inputValue) && specialChar.test(inputValue) && upperCaseLetters.test(inputValue) && numbers.test(inputValue) 
     && inputValue.length>=8 && inputValue.length<=20){
      
         $("ul li span" ).remove();
         $( "ul li" ).css("color", "green");
         $( "ul li" ).prepend(spanTick);
-    }else{
+    }
+    
+    else{
         $( "ul li" ).css("color", "red");
         $("ul li span" ).remove();
         $( "ul li" ).prepend(spanRemove);
+      
+     if(specialChar.test(inputValue) &&  upperCaseLetters.test(inputValue)){
+        $("ul li:nth-child(EVEN) span" ).remove();
+        $( "ul li:nth-child(EVEN)" ).css("color", "green");
+        $( "ul li:nth-child(EVEN)" ).prepend(spanTick);
+     }   
     if(lowerCaseLetters.test(inputValue) && inputValue.length>=8 && inputValue.length<=20 ){
        
         
          $("ul li:nth-child(1) span" ).remove();
            $( "ul li:nth-child(1)" ).css("color", "green");
+           e.preventDefault();
            $( "ul li:nth-child(1)" ).prepend(spanTick);
+         
       
        }
         if(specialChar.test(inputValue)){
           
            $( "ul li:nth-child(4) span" ).remove();
            $( "ul li:nth-child(4)" ).css("color", "green");
+           e.preventDefault();
            $( "ul li:nth-child(4)" ).prepend(spanTick);
+           
       }
-       if(upperCaseLetters.test(inputValue)){
+       if( upperCaseLetters.test(inputValue)){
            
            $( "ul li:nth-child(2) span" ).remove();
            $( "ul li:nth-child(2)" ).css("color", "green");
+           e.preventDefault();
            $( "ul li:nth-child(2)" ).prepend(spanTick);
+           
            
        }
         if(numbers.test(inputValue) ) { 
       
        $( "ul li:nth-child(3) span" ).remove();
        $( "ul li:nth-child(3)" ).css("color", "green");
+       e.preventDefault();
        $( "ul li:nth-child(3)" ).prepend(spanTick);
+     
        
       }
+      
     }
    
 
@@ -13399,7 +13417,7 @@ $(document).ready(function(){
       });
 });
 $( document ).ready(function() {
-    
+    $( ".pass-input ul li" ).css("list-style-type", "none");
     $('#cnfpasswordSignUp').bind("cut copy paste", function(e) {
         e.preventDefault();
        
@@ -13436,28 +13454,22 @@ if(!validatePhone(email)&&(!validateEmail(email)))
   });
 
 
-$("#passwordSignUp").keyup(function(){
+$("#passwordSignUp").keyup(function(e){
 
     var passWord=$("#passwordSignUp").val();
 
-     changeTooltipColor(passWord);
+     changeTooltipColor(passWord,e);
      checkPasswordStrength(passWord);
 });
 
 $("#passwordSignUp").on('input',function(e){
     var passWordChk=$("#passwordSignUp").val();
     if(!validatePassword(passWordChk)){
-  
         $(".invalidPassErr").html("Invalid Password ");
     }else{
         $(".invalidPassErr").html(" ");
     }
  });
-
-
-
-
-
 
 
 // Empty Fields Validation
