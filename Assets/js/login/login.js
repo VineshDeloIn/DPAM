0544c7b032f5f3e702bc95eb5bac3a522663dd90
Surendra <sanushka@deloitte.com>
2018-09-06 17:10:51 +0530

    final login --anushka

--- Assets/js/login/login.js
+++ Assets/js/login/login.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+    $('#loginBtn').on('click',function () {
+        // alert('hi')
+        $('.landing-image').addClass("clicked");
+        $('.landing-content').addClass("slideadd");
+        $('#loginBtn:visible').hide();
+        $('#SignBtn:visible').hide();
+    });
+
+    $('#SignBtn').on('click',function () {
+        // alert('hi')
+        $('.landing-image').addClass("clicked");
+        $('.landing-content').addClass("slideadd");
+        $('#loginBtn:visible').hide();
+        $('#SignBtn:visible').hide();
+    });
+
+    $('#landingSideCloseBtn').on('click',function(){
+        $('.landing-image').removeClass("clicked");
+        $('.landing-content').removeClass("slideadd");
+        $('#loginBtn:hidden').show();
+        $('#SignBtn:hidden').show();
+    });
+
     $("#uaeLogin").click(function(event){
         event.preventDefault();
         $('.alert').show();
