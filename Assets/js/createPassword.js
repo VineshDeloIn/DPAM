8312fb9bdb243cfad3ac05dafaf53848a8c6284c
Raghuvanshi <praghuvanshi@deloitte.com>
2018-09-19 12:06:05 +0530

    create password final ---poonam

---
+++ Assets/js/createPassword.js
@@ -0,0 +1,77 @@
+$( document ).ready(function() {
+  
+    $('.confirm-password-create').bind("cut copy paste", function(e) {
+        e.preventDefault();
+       
+        });
+    
+  $('.create-show-password').click(function() {
+    passtoTextCreate();
+  
+  });
+
+
+$(".password-create").keyup(function(e){
+
+    var passWord=$(".create-show-password").val();
+    
+    checkPasswordStrengthCreate(passWord);
+});
+
+$(".confirm-password-create").keyup(function(e){
+
+    var passWord=$(".create-show-password").val();
+    
+    checkPasswordStrengthCreate(passWord);
+});
+
+
+
+
+
+$( ".create-password-form" ).on('submit',function( event ) {
+    
+    
+        var passwordCreate = $(".password-create").val();
+        var cnfpasswordCreate = $(".confirm-password-create").val();
+        
+        if(!comparePassword(passwordCreate,cnfpasswordCreate)){
+            
+            $(".create-unequal-pswrd-err").html("Passwords do not match");
+            isPasswordValid = false;
+        }
+       
+
+        //Checking if password is not empty
+        if(passwordCreate == "") {
+            $(".create-pswrd-err").html("Password Cannot Be Empty!");
+            isPasswordValid = false;
+        } else {
+            isPasswordValid = true;
+        }
+
+       
+        
+        if(!validatePassword(passwordCreate)){
+  
+            $(".create-invalid-pswrd-err").html("Invalid Password ");
+            isPasswordValid = false;
+     }
+     if(cnfpasswordCreate==""){
+      
+            $(".create-cnfpswrd-err").html("Invalid Password");
+            isPasswordValid = false;
+     }
+    
+     if(isPasswordValid) {
+        $(".create-password-form").submit();
+     } 
+    event.preventDefault();
+   
+       
+    });
+    
+
+});
+
+
