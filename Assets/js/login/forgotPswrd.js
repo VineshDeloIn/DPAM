7249c4088b730232fd0f918f752493e95f86e542
Surendra <sanushka@deloitte.com>
2018-09-20 17:10:56 +0530

    --anushka

--- Assets/js/login/forgotPswrd.js
+++ Assets/js/login/forgotPswrd.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
    
 
     $( ".forgtPswrdForm" ).on('submit',function( event ) {        
-         
+        //  event.preventDefault();
         var emailMob = $("#forgtPswrdMobMail").val();
 
 
